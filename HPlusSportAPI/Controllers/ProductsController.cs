86eb7314d33ebaa9bf02793479a042045467986c
Daddy-Gats <charalamposgatsakos@gmail.com>
2022-03-27 15:04:09 +0200

    Retieving Data Chap End

--- HPlusSportAPI/Controllers/ProductsController.cs
+++ HPlusSportAPI/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HPlusSportAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,30 @@ namespace HPlusSportAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
         [HttpGet]
-        public string GetProducts() {
-            return "Ok.";
+        public async Task<IActionResult> GetAllProducts()
+        {
+            return  Ok(await _context.Products.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var projuct = await _context.Products.FindAsync(id);
+            if(projuct == null)
+            {
+                return NotFound();
+            }
+            return Ok(projuct);
         }
     }
 }
